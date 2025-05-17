17fbb025019d152abe426cb5cfb77e59061f2b78
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-17 19:41:21 +0800

    added class

---
+++ class1.py
@@ -0,0 +1,14 @@
+class Person:
+    name = "lilly"
+    age = 28
+    city = "shanghai"
+    phone = "18510656167"
+    def fn(self):
+        return 'unknow'
+hu = Person()
+
+print(hu.fn())
+print(hu.age)
+print(hu.name)
+print(hu.phone)
+print(hu.city)
