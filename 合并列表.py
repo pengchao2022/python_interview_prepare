bc0fbb7de3e15d7990b3b39fcae172f3cb9caad9
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-17 13:29:59 +0800

    extend

---
+++ 合并列表.py
@@ -0,0 +1,12 @@
+a = [5, 7, 9, 56]
+
+b = [77, 67, 43, 9, 90]
+
+result = a + b
+
+
+print(result)
+
+a.extend(b)
+
+print(a)
---
+++ 生成列表.py
@@ -0,0 +1,31 @@
+#如何将 "1,2,3" 变成 ["1","2","3"]
+
+
+s = "1,2,3"
+
+list1 = s.split(',')
+
+#char_list1 = list(s)
+
+#print(char_list1)
+print(list1)
+
+
+s1 = "Hellokity"
+
+char_list = list(s1)
+
+print(char_list)
+
+
+s3 = "I Love China"
+
+char_list2 = list(s3)
+
+print(char_list2)
+
+s4 = "I love China"
+
+list4 = s4.split(" ")
+
+print(list4)
