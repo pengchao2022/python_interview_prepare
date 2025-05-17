b8e8a8ba669f8414d6579bfa872b3c7004629927
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-17 13:55:13 +0800

    added the random

---
+++ 生成一个随机数.py
@@ -0,0 +1,29 @@
+import random
+
+#随机生成一个小数
+a = random.random()
+
+print(a)
+
+#随机生成一个0-9之间得数
+b = random.randint(0,9)
+
+print(b)
+
+#随机生成一个6位数
+c = random.randint(100000, 999999)
+
+print(c)
+
+#随机生成一个4位数
+d = random.randint(1000, 9999)
+
+print(d)
+
+#随机生成一个11位数
+e = random.randint(10000000000, 99999999999)
+print(e)
+
+
+
+
