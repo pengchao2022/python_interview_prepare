fdcfffb9744a1d7e3b0ffce19600363168797779
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-18 20:21:41 +0800

    added list

---
+++ 列表元素打乱练习.py
@@ -0,0 +1,24 @@
+
+import random
+
+mylist = [66, 8, 90, 45, 56, 8, 55, 34, 200]
+
+random.shuffle(mylist)
+
+print(mylist)
+
+list1 = [4, 8, 11]
+
+random.shuffle(list1)
+
+print(list1)
+
+list2 = [99, 76, 208]
+
+random.shuffle(list2)
+
+print(list2)
+
+list3 = ['kate', 'lily', 'tim']
+random.shuffle(list3)
+print(list3)
---
+++ 打乱一个排好序的列表list.py
@@ -0,0 +1,9 @@
+#将有序的列表元素打乱
+
+import random
+
+mylist = [98, 67, 65, 8, 44, 65536, 20, 1]
+
+random.shuffle(mylist)
+
+print(mylist)
