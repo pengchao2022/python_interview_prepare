2f14a9e2346275aebe2ca71efa8b90631a19ea6f
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-18 21:45:27 +0800

    added list

---
+++ 列表生成式等差数列.py
@@ -0,0 +1,14 @@
+
+#写公差为11 的等差数列
+
+list1 = list(range(0,100,11)) #公差为11 的等差数列
+
+list2 = list(range(100,200,8)) #公差为8的等差数列
+
+list3 = [x*11 for x in range(0,10)]
+
+print(list3)
+
+print(list1)
+
+print(list2)
