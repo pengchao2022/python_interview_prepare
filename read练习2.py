e15840a9c68ad3fb52e7f80bf313cf801f047e81
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-19 08:40:13 +0800

    updated

--- read练习2.py
+++ read练习2.py
@@ -4,6 +4,8 @@ with open('myfile.txt', 'r', encoding='utf-8') as fp:
 
     print(fp.read(30))
 
-    #print(fp.readline())
+    #print(fp.readline())  readline每次读取一行，里面不用写参数
 
-    #print(fp.readlines())
+    #print(fp.readlines()) readlines 读取所有行
+
+    
