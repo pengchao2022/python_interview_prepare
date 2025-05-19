a0758375d5cb386d306571b13ef4d896dec825a6
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-19 20:28:11 +0800

    added

--- 变量赋值方式1.py
+++ 变量赋值方式1.py
@@ -10,11 +10,28 @@ print(d)
 
 '''
 
+'''
 a, b, c, d = 87, 99, 53, 100
 
 print(a)
 print(b)
 print(c)
 print(d)
+'''
+
+a, b, c, d = 2, 2, 1000, 1000
+
+print(a is b, c is d)
+
+def foo():
+    e = 2000
+    f = 2000
+    print(e is f, e is d)
+
+    g = 2
+    print(g is a)
+
+
+foo()
 
 
