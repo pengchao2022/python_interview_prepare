843a6ea56c6863d0e32b45e32639120d65365c7d
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-19 08:08:38 +0800

    updated

--- read-readline练习1.py
+++ read-readline练习1.py
@@ -1,8 +1,8 @@
 
 with open('file.txt', 'r', encoding='utf-8') as fp:
-     print(fp.read())
+     print(fp.read()) #读取文本的全部内容
 
-    #print(fp.readline())
+     print(fp.readline()) #读取文本的一行
 
-    #print(fp.readlines())
+     print(fp.readlines()) #读取文本全部 包括空格
 
---
+++ read练习2.py
@@ -0,0 +1,9 @@
+
+with open('myfile.txt', 'r', encoding='utf-8') as fp:
+    print(fp.read(30))
+
+    print(fp.read(30))
+
+    #print(fp.readline())
+
+    #print(fp.readlines())
