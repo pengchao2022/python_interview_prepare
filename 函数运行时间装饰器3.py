0fe2d1563206edec01cd8ca39ee53e124a5c16db
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-19 09:09:16 +0800

    added outer

---
+++ 函数运行时间装饰器3.py
@@ -0,0 +1,17 @@
+#编写一个判断函数运行时间的装饰器
+
+import time
+
+def outer(fn):
+    def wrapper(*arg, **kwargs):
+        start = time.time()
+        ret = fn(*arg, **kwargs)
+        end = time.time()
+        print(end -start)
+    return wrapper
+
+@outer
+def test():
+    time.sleep(3)
+
+test()
