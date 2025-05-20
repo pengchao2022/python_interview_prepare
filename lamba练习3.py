3a1b35c7fb0a01ab5c7521d1f81df3bff8c210ac
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-20 09:14:29 +0800

    added

---
+++ lamba练习3.py
@@ -0,0 +1,20 @@
+
+num_desc = lambda x: "偶数" if x % 2 == 0 else "奇数"
+
+print(num_desc(3))
+
+squre = lambda x: x ** 2
+
+print(squre(9))
+
+lifang = lambda y: y ** 3
+
+print(lifang(3))
+
+sicifang = lambda z: z ** 4
+
+print(sicifang(2))
+
+saner = lambda k: k ** 32
+
+print(saner(2))
