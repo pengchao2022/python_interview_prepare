5cf4af107d817cb4dd92d9724b0ff402b3b868c1
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-21 18:39:06 +0800

    added

---
+++ 函数进行字符串反转.py
@@ -0,0 +1,15 @@
+
+def reverse_str(arg):
+
+    new_rev_str = arg[::-1]
+
+    print(f"反转后的字符串为：{new_rev_str}")
+
+
+str1 = "Iloveyou"
+
+str2 = "I Love China and family!"
+
+reverse_str(str1)
+
+reverse_str(str2)
---
+++ 字符串反转1.py
@@ -0,0 +1,5 @@
+str1 = 'IloveChina'
+
+rev_str1 = str1[:: -1]
+
+print(rev_str1)
