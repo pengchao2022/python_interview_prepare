0a0695af3f2068a38aa314e99f36748bb824c82f
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-22 08:39:28 +0800

    added

---
+++ python格式化1.py
@@ -0,0 +1,23 @@
+
+name = 'Alice'
+age = 32
+height = 175
+weight = 55.9
+
+print("姓名为：%s" % name)
+
+print("年龄为：%d" % age)
+
+print("身高为： %d" % height)
+
+print("体重为：%.2f" % weight)
+
+#方法二
+
+print(f"姓名为：{name}")
+
+print(f"年龄为：{age}")
+
+print(f"身高为：{height}")
+
+print(f"体重为：{weight}")
