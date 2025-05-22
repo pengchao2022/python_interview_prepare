91c7504f5ba306852eac90c2f30d3e4b8ee987fd
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-05-22 19:35:26 +0800

    added

---
+++ 生成列表一行代码1.py
@@ -0,0 +1,10 @@
+
+# 生成一个偶数列表
+
+even_list = [x for x in range(201) if x % 2 == 0]
+
+print(even_list)
+print()
+
+odd_list = [y for y in range(201) if y % 2 == 1]
+print(odd_list)
---
+++ 生成列表简洁.py
@@ -0,0 +1,10 @@
+
+#生成偶数列表
+even_list = [x for x in range(201) if x % 2 == 0]
+print(even_list)
+print()
+
+#生成奇数列表
+odd_list = [y for y in range(201) if y % 2 == 1]
+print(odd_list)
+
